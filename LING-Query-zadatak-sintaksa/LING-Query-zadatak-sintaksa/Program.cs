9a7f1860f4ac5ddf38ee4be7f6e340de410b89bb
Mannequin52 <92926451+Mannequin52@users.noreply.github.com>
2023-09-18 10:45:40 +0200

    Add files via upload

---
+++ LING-Query-zadatak-sintaksa/LING-Query-zadatak-sintaksa/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        // Student collection
+        List<Student> studentList = new List<Student>() {
+                new Student() { StudentID = 1, StudentName = "John" } ,
+                new Student() { StudentID = 2, StudentName = "Moin" } ,
+                new Student() { StudentID = 3, StudentName = "Bill" } ,
+                new Student() { StudentID = 4, StudentName = "Ram" } ,
+                new Student() { StudentID = 5, StudentName = "Ron"  }
+            };
+
+        // upit i izvršavanje upita
+
+        var result = from s in studentList 
+                     select s;
+
+        foreach(var name in result)
+        {
+            Console.WriteLine(name.StudentName);
+        }
+
+        var res = studentList.Select(s => s);
+
+        foreach(var name in res)
+        {
+            Console.WriteLine(name.StudentName);
+        }
+        
+
+        Console.ReadKey();
+    }
+}
+
+
+public class Student
+{
+
+    public int StudentID { get; set; }
+    public string StudentName { get; set; }
+}
